16cc0dc0ee3ea94033a60242dbffc8da50031f34
Steven Robertson <steverobertson@gmail.com>
2018-06-18 20:25:19 +0100

    Removed redundant comment

--- openmp.c
+++ openmp.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
-/*
-static int wc(char *docname){
-    char *doc = string_from_file(docname);
-    if (!doc) return 0;
-    char *delimiters = " `~!@#$%^&*()_-+={[]}|\\;:\",<>./?\n";
-    ok_array *words = ok_array_new(doc, delimiters);
-    if (!words) return 0;
-    double out= words->length;
-    ok_array_free(words);
-    return out;
-}
-*/
-
 void do_openmp_test() {
     #pragma omp parallel for
     for (int i = 0; i < 10; i++) {
