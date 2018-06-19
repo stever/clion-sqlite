9d49097263132da9a039d07b5ea9bc9dc177c668
Steven Robertson <steverobertson@gmail.com>
2018-06-19 09:50:31 +0100

    C# program to attempt to load DLL

---
+++ src/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    internal static class Program
+    {
+        [DllImport(@"C:\Users\Steve\Desktop\Source\Bitbucket\clionsqlite\libs\libCLionSQLite.dll")]
+        private static extern void hello_library();
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Calling library function");
+            hello_library();
+            Console.WriteLine("Finished");
+        }
+    }
+}
